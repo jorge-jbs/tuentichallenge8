59edeef90507fa2a0315682b848d673abe776d6b
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-04-27 17:38:20 +0200

    Primer boceto antes de entregar 3

--- 3.c
+++ 3.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -20,6 +21,37 @@ void genNotesSet(int root, const tScale mode, tNotesSet set) {
         set[root % 12] = true;
         root += mode[i];
     }
+}
+
+void scan_note(int *note) {
+    char s[3] = {0};
+    scanf("%s", s);
+    switch (s[0]) {
+    case 'A': *note = 0; break;
+    case 'B': *note = 2; break;
+    case 'C': *note = 3; break;
+    case 'D': *note = 5; break;
+    case 'E': *note = 7; break;
+    case 'F': *note = 8; break;
+    case 'G': *note = 10; break;
+    }
+    *note += 12;
+    switch (s[1]) {
+    case '#': ++(*note); break;
+    case 'b': --(*note); break;
+    }
+    *note %= 12;
+
+
+    /*
+    char c;
+    scanf("%c", &c);
+    switch (c) {
+    case 'M': *mode = Major; break;
+    case 'm': *mode = Minor; break;
+    }
+    scanf("%c", &c);
+    */
 }
 
 void print_note(int note) {
@@ -50,7 +82,69 @@ bool is_match(int root, int notes[1000], int n, const tScale mode) {
     return true;
 }
 
+void find_scales(tNotesSet scales, const tScale mode, int notes[1000], int n, int *k) {
+    *k = 0;
+    for (int root = 0; root < 12; root++) {
+        if (is_match(root, notes, n, mode)) {
+            printf("M");
+            print_note(root);
+            printf(" ");
+            ++(*k);
+        }
+    }
+}
+
 int main() {
+    int c;
+    scanf("%d\n", &c);
+    for (int i = 0; i < c; i++) {
+        printf("Case #%d: ", i+1);
+        int n;
+        scanf("%d\n", &n);
+        int notes[1000];
+        for (int j = 0; j < n; j++) {
+            scan_note(&notes[j]);
+        }
+        int *majors = malloc(sizeof(int)*12);
+        int a = 0;
+        for (int root = 0; root < 12; root++) {
+            if (is_match(root, notes, n, MAJOR)) {
+                majors[a] = root;
+                a++;
+            }
+        }
+        int *minors = malloc(sizeof(int)*12);
+        int b = 0;
+        for (int root = 0; root < 12; root++) {
+            if (is_match(root, notes, n, MINOR)) {
+                minors[b] = root;
+                b++;
+            }
+        }
+        int j;
+        for (j = 0; j < a-1; j++) {
+            printf("M");
+            print_note(majors[j]);
+            printf(" ");
+        }
+        if (a != 0) {
+            printf("M");
+            print_note(majors[j]);
+            if (b != 0) printf(" ");
+        }
+        for (j = 0; j < b-1; j++) {
+            printf("m");
+            print_note(minors[j]);
+            printf(" ");
+        }
+        if (b != 0) {
+            printf("m");
+            print_note(minors[j]);
+        }
+        if (a == 0 && b == 0) printf("None");
+        printf("\n");
+    }
+
     /*
     tScale scale;
     genScale(0, MINOR, scale);
@@ -60,9 +154,11 @@ int main() {
     }
     printf("\n");
     */
-    for (int i = 0; i < 24000*10000; i++) {
-    }
-    int notes[1000];
+    /*
+    tMode mode;
+    int note;
+    scan_note(&mode, &note);
+    printf("%d %d\n", mode, note);
     notes[0] = 3;
     notes[1] = 3;
     notes[2] = 10;
@@ -70,5 +166,6 @@ int main() {
     notes[4] = 0;
     notes[5] = 0;
     notes[6] = 10;
-    printf("%d\n", is_match(2, notes, 7, MAJOR));
+    */
+    //printf("%d\n", is_match(2, notes, 7, MAJOR));
 }
