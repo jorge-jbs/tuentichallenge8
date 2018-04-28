d4f9a4a74f9451636c6bb1e99ad635a9989fca4e
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-04-28 19:41:31 +0200

    Casi listo para publicar

--- 6.cpp
+++ 6.cpp
@@ -9,60 +9,74 @@ struct tSchedule {
 };
 
 bool operator<(tSchedule a, tSchedule b) {
-    return a.release < b.release;
+    if (a.release < b.release) {
+        return true;
+    } else if (a.release > b.release) {
+        return false;
+    }
+    return a.press < b.press;
 }
 
-int best_score(map<tSchedule, int> blocks, auto last_block, int points) {
-    //printf("%d ", points);
-    auto it = last_block;
-    ++it;
-    while (it != blocks.end()) {
-        cout << points << endl;
-        if (last_block->first.release <= it->first.press) {
-            points = max(points, best_score(blocks, it, it->second + points));
-        }
-        it++;
-    }
-    return points;
+bool operator==(tSchedule a, tSchedule b) {
+    return a.press == b.press && a.release == b.release;
 }
 
-/*
-int best_score(map<tSchedule, int> blocks, int last_block, int time, int points) {
-    int i = last_block + 1;
-    while (i < blocks.size()) {
-        if ((blocks.begin() + last_block).first.release <= (blocks.begin() + i).first.press) {
-            points = max(points, best_score(blocks, i, blocks[i].release, blocks[i].second + points))
+int best_score(vector<pair<tSchedule, int> > blocks, int last_block, int points) {
+    int best = points;
+    for (int i = last_block+1; i < blocks.size(); i++) {
+        //printf("points: %d; best: %d;\n", points, best);
+        if (blocks[last_block].first.release < blocks[i].first.press) {
+            //printf("%d %d\n", last_block, i);
+            //cout << i << endl;
+            //printf("{{{\n");
+            best = max(best, best_score(blocks, i, blocks[i].second + points));
+            //printf("}}}\n");
         }
-        i++;
     }
-    return points;
+    return best;
 }
-*/
+
+int best_best_score(vector<pair<tSchedule, int> > blocks) {
+    int score = 0;
+    for (int i = 0; i < blocks.size(); i++) {
+        score = max(score, best_score(blocks, i, blocks[i].second));
+        //cout << endl;
+    }
+    return score;
+}
 
 int main() {
     int c;
     scanf("%d", &c);
     for (int i = 0; i < c; i++) {
-        cout << "hue" << endl;
         int n;
         scanf("%d", &n);
-        map<tSchedule, int> blocks;
+        vector<pair<tSchedule, int> > blocks;
         for (int j = 0; j < n; j++) {
-            cout << "huo " << j << " " << n << endl;
             int x, l, s, p;
             scanf("%d %d %d %d", &x, &l, &s, &p);
-            cout << "1" << endl;
             tSchedule sched = { .press = x/s, .release = (x+l)/s };
-            cout << "2" << endl;
-            auto em = blocks.emplace(pair<tSchedule, int>(sched, p));
-            cout << "3" << endl;
-            if (!em.second) {
-                cout << "4" << endl;
-                em.first->second += p;
-            }
-            cout << "5" << endl;
+            //if (i != 52) continue;
+            //printf("%d %d\n", x/s, (x+l)/s);
+            blocks.push_back(pair<tSchedule, int>(sched, p));
         }
-        cout << "hola" << endl;
-        printf("%d\n", best_score(blocks, blocks.begin(), 0));
+        //if (i != 52) continue;
+        sort(blocks.begin(), blocks.end());
+        for(int j = 0; j < blocks.size() - 1; j++) {
+            if (blocks[j].first == blocks[j+1].first) {
+                blocks[j].second += blocks[j+1].second;
+                blocks.erase(blocks.begin() + j + 1);
+                j--;
+            }
+        }
+        /*
+        printf("\n");
+        for (auto b : blocks) {
+            printf("%d %d %d\n", b.first.press, b.first.release, b.second);
+        }
+        */
+        //cout << endl << "huee" << endl;
+        printf("Case #%d: %d\n", i+1, best_best_score(blocks));
+        //cout << endl;
     }
 }
