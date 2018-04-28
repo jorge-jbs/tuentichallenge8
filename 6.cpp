93f96cb7644aa9b3eac98c945872847c7eac015c
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-04-28 20:18:18 +0200

    Listo para publicar, pero demasiado lento :(

--- 6.cpp
+++ 6.cpp
@@ -24,13 +24,8 @@ bool operator==(tSchedule a, tSchedule b) {
 int best_score(vector<pair<tSchedule, int> > blocks, int last_block, int points) {
     int best = points;
     for (int i = last_block+1; i < blocks.size(); i++) {
-        //printf("points: %d; best: %d;\n", points, best);
         if (blocks[last_block].first.release < blocks[i].first.press) {
-            //printf("%d %d\n", last_block, i);
-            //cout << i << endl;
-            //printf("{{{\n");
             best = max(best, best_score(blocks, i, blocks[i].second + points));
-            //printf("}}}\n");
         }
     }
     return best;
@@ -40,7 +35,6 @@ int best_best_score(vector<pair<tSchedule, int> > blocks) {
     int score = 0;
     for (int i = 0; i < blocks.size(); i++) {
         score = max(score, best_score(blocks, i, blocks[i].second));
-        //cout << endl;
     }
     return score;
 }
@@ -56,11 +50,8 @@ int main() {
             int x, l, s, p;
             scanf("%d %d %d %d", &x, &l, &s, &p);
             tSchedule sched = { .press = x/s, .release = (x+l)/s };
-            //if (i != 52) continue;
-            //printf("%d %d\n", x/s, (x+l)/s);
             blocks.push_back(pair<tSchedule, int>(sched, p));
         }
-        //if (i != 52) continue;
         sort(blocks.begin(), blocks.end());
         for(int j = 0; j < blocks.size() - 1; j++) {
             if (blocks[j].first == blocks[j+1].first) {
@@ -69,14 +60,6 @@ int main() {
                 j--;
             }
         }
-        /*
-        printf("\n");
-        for (auto b : blocks) {
-            printf("%d %d %d\n", b.first.press, b.first.release, b.second);
-        }
-        */
-        //cout << endl << "huee" << endl;
         printf("Case #%d: %d\n", i+1, best_best_score(blocks));
-        //cout << endl;
     }
 }
