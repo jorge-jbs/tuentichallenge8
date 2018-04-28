e070c3e59ed7fb2457e2d49a39d412f4ea2b3c41
Jorge Blázquez Saborido <jorge.jbs@protonmail.com>
2018-04-28 18:18:28 +0200

    Versión publicada

--- 5.cpp
+++ 5.cpp
@@ -13,7 +13,6 @@ bool is_match(string a, int n, string b, int m) {
     int asize = a.size();
     int bsize = b.size();
     for (int i = n, j = m; i < asize && j < bsize; i++, j++) {
-        //printf("%c<->%c", a[i], b[i]);
         if (a[i] != b[j]) {
             return false;
         }
@@ -31,26 +30,9 @@ bool is_in(vector<int> xs, int y) {
 bool orig(vector<int> & a, vector<int> & b, vector<string> parts) {
     int alen = size(a, parts);
     int blen = size(b, parts);
-    //printf("%d %d", alen, blen);
-    //cout << endl;
     if (alen == blen && alen != 0) {
-        /*
-        a.insert(a.end(), b.begin(), b.end());
-        sort(a.begin(), a.end());
-        return a;
-        */
         return true;
     } else {
-        /*
-        if (alen >= blen) {
-            vector<int> aux = b;
-            b = a;
-            a = aux;
-            int auxlen = blen;
-            blen = alen;
-            alen = auxlen;
-        }
-        */
         int dif = abs(blen - alen);
         string last;
         if (alen < blen) {
@@ -67,9 +49,7 @@ bool orig(vector<int> & a, vector<int> & b, vector<string> parts) {
                     b.push_back(i);
                 }
                 if (is_match(parts[i], 0, last, last.size() - dif)) {
-                    //cout << "hola " << i << endl;
                     if (orig(a, b, parts)) {
-                        //cout << "nos vamos" << endl;
                         return true;
                     }
                 }
@@ -84,44 +64,22 @@ bool orig(vector<int> & a, vector<int> & b, vector<string> parts) {
     }
 }
 
-/*
-pair<vector<int>, vector<int> > cont(vector<int> a, vector<int> b, vector<string> parts) {
-    int alen = size(a);
-    int blen = size(b);
-    if (alen < blen) {
-        int dif = blen - alen;
-        string last = parts[b.back()];
-        for (auto p : parts) {
-            //if (p `is not in a or b`) {
-
-            //}
-        }
-    } else {
-    }
-}
-
-vector<int> orig(vector<string> parts) {
-    int len = parts.size();
-    for (int i = 0; i < len; i++) {
-    }
-}
-*/
-
-
 int main() {
     string s;
     getline(cin, s);
 
+    // All messages are passed to this program. It is its responsibility to
+    // discard info messages.
     if (s[0] == '>') return 0;
 
     stringstream ss(s);
 
-    string item;
-    vector<string> tokens;
-    while (getline(ss, item, ' ')) {
-        tokens.push_back(item);
+    string part;
+    vector<string> parts;
+    while (getline(ss, part, ' ')) {
+        parts.push_back(part);
     }
-    //cout << tokens[1];
+
     vector<int> a;
     vector<int> b;
 
@@ -131,7 +89,7 @@ int main() {
         b.pop_back();
         b.push_back(i);
         i++;
-    } while (!orig(a, b, tokens));
+    } while (!orig(a, b, parts));
 
     a.insert(a.end(), b.begin(), b.end());
     sort(a.begin(), a.end());
@@ -142,6 +100,4 @@ int main() {
     printf("%d", a[i]+1);
     cout << endl;
     return 0;
-
-
 }
